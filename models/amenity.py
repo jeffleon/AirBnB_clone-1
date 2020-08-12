a0c321c5071c18ef625fd50991eae36cfd3ef09e
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-08-12 16:43:22 +0000

    [Update]Task 10

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+import models
+import sqlalchemy
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.orm import relationship
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    __tablename__ = "amenities"
+    name = Column(String(128))
+    place_amenities = relationship("Place", secondary="place_amenity")
--- models/place.py
+++ models/place.py
@@ -5,18 +5,60 @@ import models
 import sqlalchemy
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, ForeignKey, String, Float, Integer
+from sqlalchemy import *
+from sqlalchemy.orm import relationship
+
+
+place_amenity = Table("place_amenity", Base.metadata,
+                      Column("place_id", String(60), ForeignKey("places.id"),
+                             primary_key=True, nullable=False),
+                      Column("amenity_id", String(60), ForeignKey\
+                             ("amenities.id"), primary_key=True,
+                             nullable=False))
+
 
 class Place(BaseModel, Base):
     """ A place to stay """
-    __tablename__ = "places"
-    city_id = Column(String(60), ForeignKey("cities.id"))
-    user_id = Column(String(60), ForeignKey("users.id"))
-    name = Column(String(128))
-    description = Column(String(1024))
-    number_rooms = Column(Integer, default=0)
-    number_bathrooms = Column(Integer, default=0)
-    max_guest = Column(Integer, default=0)
-    price_by_night = Column(Integer, default=0)
-    latitude = Column(Float, nullable=False)
-    longitude = Column(Float, nullable=False)
-    amenity_ids = []
+    if models.storage_type == "db":
+        __tablename__ = "places"
+        city_id = Column(String(60), ForeignKey("cities.id"))
+        user_id = Column(String(60), ForeignKey("users.id"))
+        name = Column(String(128))
+        description = Column(String(1024))
+        number_rooms = Column(Integer, default=0)
+        number_bathrooms = Column(Integer, default=0)
+        max_guest = Column(Integer, default=0)
+        price_by_night = Column(Integer, default=0)
+        latitude = Column(Float, nullable=True)
+        longitude = Column(Float, nullable=True)
+        amenity_ids = []
+        amenities = relationship("Amenity",
+                                 secondary="place_amenity",
+                                 backref="place_amenities",
+                                 viewonly=False)
+    else:
+        city_id = ""
+        user_id = ""
+        name = ""
+        description = ""
+        number_rooms = 0
+        number_bathrooms = 0
+        max_guest = 0
+        price_by_night = 0
+        latitude = 0.0
+        longitude = 0.0
+        amenity_ids = []
+
+    if models.storage_type != "db":
+        @property
+        def amenities(self):
+            """ Return a list of amenity instances based on the attribute
+amenity_ids that contains all Amenity.id linked to the Place"""
+            return self.amenity_ids
+
+        @amenities.setter
+        def amenities(self, obj):
+            """handles append method for adding an Amenity.id to the attribute
+amenity_ids."""
+            if isinstance(obj, Amenity):
+                self.amenity_ids.append(obj.id)
