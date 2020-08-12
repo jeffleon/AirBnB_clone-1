b02c0821592946ce8d3a05a042db37cac9354a54
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-08-12 17:47:44 +0000

    [Updated]Added another privileges

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -3,6 +3,8 @@
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 -- Creates an user in the database
 CREATE USER IF NOT EXISTS 'hbnb_test_db'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+-- Grants privileges
+GRANT USAGE ON *.* TO 'hbnb_test'@'localhost';
 -- Grants all privileges
 GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
 -- Grants select privileges to the user
