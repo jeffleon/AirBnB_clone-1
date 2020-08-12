7c1485c8cf65a21461108b8f11fa5d3dbb9ec836
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-08-12 17:45:44 +0000

    [Updated]Added comments

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,7 +1,11 @@
 -- script that prepares a MySQL server for the project:
-
+-- Create the database
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+-- Creates an user in the database
 CREATE USER IF NOT EXISTS 'hbnb_test_db'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+-- Grants all privileges
 GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
+-- Grants select privileges to the user
 GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
+-- reload the grant tables
 FLUSH PRIVILEGES;
