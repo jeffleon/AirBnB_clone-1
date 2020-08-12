7fee2471d1483d1f85270a3fac7256aa106b0491
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-08-12 17:56:46 +0000

    [UPDATE] task 3

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -2,6 +2,7 @@
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost';
 -- Grant privileges to user on database 
+SET PASSWORD FOR 'hbnb_dev'@'localhost' = 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
