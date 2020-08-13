331f3cd148b76ac0575a8e5dc9eec84bc05c4391
Miguel Angel Cipamocha Figueredo <60364713+MiguelCF06@users.noreply.github.com>
2020-08-13 16:44:40 -0500

    pep8

--- models/state.py
+++ models/state.py
@@ -14,7 +14,8 @@ class State(BaseModel, Base):
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
     if storage_type == "db":
-        cities = relationship("City", backref="state")
+        cities = relationship("City", backref="state",
+                              cascade="all, delete, delete-orphan")
     else:
         @property
         def cities(self):
