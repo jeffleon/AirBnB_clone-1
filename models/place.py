3230c8130d2b05acabd5f335d1070dc5fdc9d1ed
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-08-13 21:10:56 +0000

    [UPDATE]

--- models/place.py
+++ models/place.py
@@ -6,7 +6,7 @@ import sqlalchemy
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, ForeignKey, String, Float, Integer
 from sqlalchemy import *
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, backref
 import os
 
 place_amenity = Table("place_amenity", Base.metadata,
@@ -37,6 +37,11 @@ class Place(BaseModel, Base):
         amenities = relationship("Amenity",
                                  secondary="place_amenity",
                                  viewonly=False)
+        '''
+        reviews = relationship("Review",
+                               cascade="all,delete",
+                               backref=backref("place", cascade="all,delete"))
+        '''
     if storage_type != "db":
         '''
         @property
@@ -48,7 +53,7 @@ class Place(BaseModel, Base):
                 if value.place_id == self.id:
                     reviews.append(value)
             return reviews
-        '''         
+        '''
         @property
         def amenities(self):
             """ Return a list of amenity instances based on the attribute
--- models/user.py
+++ models/user.py
@@ -2,7 +2,7 @@
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Integer, String, Column, DateTime, ForeignKey
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, backref
 
 
 class User(BaseModel, Base):
@@ -12,7 +12,7 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
-    ''''
+    '''
     places = relationship("Place",
                           backref="user",
                           cascade="all, delete-orphan")
@@ -20,4 +20,3 @@ class User(BaseModel, Base):
     reviews = relationship("Review",
                            backref=backref("user",
                                            cascade="all, delete-orphan"))
-
