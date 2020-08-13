6e61ba9be3ec24297833066649ad608e9900f6ba
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-08-13 19:19:38 +0000

    [Updated]Added getter atribute for when the type of storage is filestorage

--- models/state.py
+++ models/state.py
@@ -4,10 +4,28 @@ from models.base_model import BaseModel, Base
 import sqlalchemy
 from sqlalchemy import Column, String, Integer
 from sqlalchemy.orm import relationship
+import os
+
+storage_type = os.getenv("HBNB_TYPE_STORAGE")
 
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state")
+    if storage_type == "db":
+        cities = relationship("City", backref="state")
+    else:
+        @property
+        def cities(self):
+            """
+                Return Instances City
+            """
+            all_city = models.storage.all(City)
+            state_city = []
+
+            for city in all_city.values():
+                if (city.state_id == self.id):
+                    state_city.append(city)
+
+            return (state_city)
