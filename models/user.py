9d14e53b355081eb2bc56512a6de146f3c667656
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-08-13 16:52:10 +0000

    [add] task 7

--- models/user.py
+++ models/user.py
@@ -8,10 +8,12 @@ from sqlalchemy import Column, String, Integer, Float
 from sqlalchemy.orm import relationship
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    __tablename__ = "users"
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
     places = relationship("Place", backref="user")
+    
