03766a3c22efae79f6735fc15f2d2dc52a0d701d
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-08-14 00:24:58 +0000

    [UPDATE] city

--- models/city.py
+++ models/city.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
-import sqlalchemy
 from sqlalchemy import String, ForeignKey, Column
 from sqlalchemy.orm import relationship, backref
 
@@ -9,7 +8,7 @@ from sqlalchemy.orm import relationship, backref
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = "cities"
-    state_id = Column(String(60), ForeignKey("states.id"))
-    name = Column(String(128))
+    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+    name = Column(String(128), nullable=False)
     places = relationship("Place", backref="cities",
                           cascade="all, delete")
