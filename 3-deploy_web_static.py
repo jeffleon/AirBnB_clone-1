dda277c77642588e5ba456ead4ca2a02ce78760c
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-08-18 04:54:33 +0000

    task 2 and 3

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,37 +1,63 @@
 #!/usr/bin/python3
 # function to generate a tar file
 """ generates a tar file """
-from fabric.api import *                                                           from datetime import datetime
-import os.path
-env.hosts = ['104.196.60.146','35.196.4.21']
+from fabric.api import *
+import os
+import datetime
+env.hosts = ['104.196.60.146', '35.196.4.21']
+
 
 def do_pack():
     """ This Generate a tar of /web_static """
     local("mkdir -p versions")
-    time = datetime.now().strftime("%Y%m%d%H%M%S")
+    time = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
     file = local("tar -czvf versions/web_static_%s.tgz web_static" % time)
     if file:
         return "versions/web_static_{}.tgz".format(time)
     else:
         return None
 
+
 def do_deploy(archive_path):
-    if os.path.exists(archive_path):
-        temporal_path = '/tmp/'
-        file_with_ext = archive_path.replace('versions/', '')
-        file_name = file_with_ext[:-4]
-        path_remote = "/data/web_static/releases/{}".format(file_name)
-        put(archive_path, temporal_path + file_with_ext)
-        run('mkdir -p %s' % path_remote)
-        run('tar -xzf %s%s -C %s' % (temporal_path + file_with_ext, path_remote))
-        run('rm %s%s' % (temporal_path, file_with_ext))
-        run('mv %s %s' % (path_remote + '/web_static/*', path_remote))
-        run('rm -rf %s' % path_remote + '/web_static')
-        run('rm -rf /data/web_static/current')
-        run('ln -s %s /data/web_static/current' % path_remote)
-        return True
-    else:
+    """function that distributes an archive to a server"""
+    if os.path.isfile('{}'.format(archive_path)) is False:
         return False
+    tgz_file = archive_path.split('/')[-1]
+    storage_location = '/tmp/'
+    new_location = '/data/web_static/releases/'
+    upload = put('{}'.format(archive_path), storage_location)
+    if upload is False:
+        return False
+    create_dir = run('mkdir -p {}'.format(new_location +
+                                          tgz_file.replace('.tgz', '')))
+    if create_dir is False:
+        return False
+    uncompress = run('tar -xzvf {} -C {}'.format(storage_location +
+                                                 tgz_file, new_location +
+                                                 tgz_file.replace('.tgz', '')))
+    if uncompress is False:
+        return False
+    delete_file = run('rm -f {}'.format(storage_location + tgz_file))
+    if delete_file is False:
+        return False
+    new_route = "{}".format(new_location + tgz_file.replace('.tgz', ''))
+    move_files = run('mv {}/web_static/* {}'.format(new_route, new_route))
+    if move_files is False:
+        return False
+    delete_folder = run('rm -rf {}/web_static'.format(new_route))
+    if delete_folder is False:
+        return False
+    sym_link_name = '/data/web_static/current'
+    delete_sym = run('rm -f {}'.format(sym_link_name))
+    if delete_sym is False:
+        return False
+    create_sym = run('ln -sT {} {}'.format(new_location +
+                                           tgz_file.replace('.tgz', ''),
+                                           sym_link_name))
+    if create_sym is False:
+        return False
+    return True
+
 
 def deploy():
     """ start deploy """
