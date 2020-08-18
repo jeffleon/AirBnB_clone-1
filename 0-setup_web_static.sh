d3ebad4cf553ea430ad7cb39f380edaf402d5600
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-08-18 02:07:07 +0000

    [UPDATE] task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,10 +1,13 @@
-#!/bin/env bash 
+#!/usr/bin/env bash
 # set up the information to fabric
+apt-get -y update
+apt-get -y install nginx
+service nginx start
 mkdir -p /data/web_static/releases/test
 mkdir -p /data/web_static/shared/
 touch /data/web_static/releases/test/index.html
-echo "test Holberton School" | tee /data/web_static/releases/test/index.html
+echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
-chown ubuntu:ubuntu /data/
+chown -R ubuntu:ubuntu /data/
 sed -i '/listen 80 default_server;/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
 service nginx restart
