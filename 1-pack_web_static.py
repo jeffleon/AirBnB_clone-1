2e813fff9bc4b569ed76828af61d88967413d17b
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-08-18 02:58:07 +0000

    [update] task 1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
-# function to generate a tar file 
+# function to generate a tar file
+""" generates a tar file """
 from fabric.api import local
 from datetime import datetime
+
 
 def do_pack():
     """ This Generate a tar of /web_static """
     local("mkdir -p versions")
     time = datetime.now().strftime("%Y%m%d%H%M%S")
-    local("tar -czvf versions/web_static_%s.tgz web_static" % time)
+    file = local("tar -czvf versions/web_static_%s.tgz web_static" % time)
+    if file:
+        return "versions/web_static_{}.tgz".format(time)
+    else:
+        return None
