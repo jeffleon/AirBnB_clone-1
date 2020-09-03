de2378566d4c986c14bc788e8d314fb1d604c562
root <jefferson.leon@usantotomas.edu.co>
2020-09-03 03:25:55 +0000

    add task 9

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""" return a hello HBNB """
+from flask import Flask, render_template, g
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/cities_by_states')
+def static_list():
+    states = storage.all(State)
+    obj = list(states.values())
+    return render_template("8-cities_by_states.html", states=obj)
+
+
+@app.teardown_appcontext
+def teardown_db(obj):
+    storage.close()
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
